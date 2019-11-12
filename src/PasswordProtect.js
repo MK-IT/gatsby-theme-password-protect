03e206566f8b7ce32e57c0c26aab05c337f5fe88
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2019-11-12 03:14:52 +0200

    Implement PasswordProtect component which prompts users for password and stores it as a cookie

--- src/PasswordProtect.js
+++ src/PasswordProtect.js
@@ -1,5 +1,115 @@
-import React from 'react'
+/**
+ * Write-only the password as cookie
+ */
+import React, { useState } from 'react';
+import { setSessionPassword } from './utils';
 
-export default () => {
-  return <div>PasswordProtect</div>
-}
+const styles = {
+  wrapper: {
+    height: '100vh',
+    background: '#424242',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    width: '100%',
+    height: '48px',
+    borderRadius: '4px'
+  },
+  button: {
+    width: '100%',
+    height: '48px',
+    background: 'rebeccapurple',
+    color: '#fff',
+    border: '1px solid rebeccapurple',
+    borderRadius: '4px',
+    marginTop: '16px',
+    textTransform: 'uppercase',
+    fontWeight: '300',
+    fontFamily: 'sans-serif'
+  },
+  buttonHover: {
+    background: '#fff',
+    color: '#000000'
+  },
+  link: {
+    color: '#fff',
+    fontFamily: 'sans-serif'
+  },
+  linkHover: {
+    color: 'dodgerblue'
+  }
+};
+
+const PasswordProtect = () => {
+  const [password, setPassword] = useState('');
+  const [isButtonHovered, buttonHover] = useState(false);
+  const [isPluginHovered, pluginHover] = useState(false);
+  const [isSiteHovered, siteHover] = useState(false);
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setSessionPassword(password);
+    window.location.reload(); // eslint-disable-line
+  };
+
+  return (
+    <div style={styles.wrapper}>
+      <h1 style={{ color: '#fff' }}>Welcome</h1>
+      <h4 style={{ color: '#fff' }}>Enter Password</h4>
+
+      <form onSubmit={onSubmit} style={{ width: '320px' }}>
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+          style={styles.input}
+        />
+
+        <button
+          type="submit"
+          style={{
+            ...styles.button,
+            ...(isButtonHovered ? styles.buttonHover : null)
+          }}
+          onMouseEnter={() => buttonHover(true)}
+          onMouseLeave={() => buttonHover(false)}
+        >
+          Enter
+        </button>
+      </form>
+
+      <a
+        href="https://gitlab.com/mkit/open-source/gatsby-plugin-password-protect#readme"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          ...styles.link,
+          ...(isPluginHovered ? styles.linkHover : null)
+        }}
+        onMouseEnter={() => pluginHover(true)}
+        onMouseLeave={() => pluginHover(false)}
+      >
+        gatsby-plugin-password-protect
+      </a>
+      <a
+        href="https://mkit.io"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          ...styles.link,
+          ...(isSiteHovered ? styles.linkHover : null)
+        }}
+        onMouseEnter={() => siteHover(true)}
+        onMouseLeave={() => siteHover(false)}
+      >
+        MK IT
+      </a>
+    </div>
+  );
+};
+
+export default PasswordProtect;
