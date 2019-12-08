25e8217cfb72af55e5f67978c45abe7f6dc0f0b7
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2019-12-09 00:59:53 +0200

    Page-level protection option and no password prompt for empty string as option

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,16 +3,37 @@
  * Read-only the password from cookies
  */
 import React from 'react';
-import { getSessionPassword, getQueryPassword } from './src/utils/utils';
+import {
+  getSessionPassword,
+  getQueryPassword,
+  isProtectedPage
+} from './src/utils/utils';
 import PasswordProtect from './src/components/PasswordProtect';
 
-export const wrapPageElement = ({ props }, themeOptions) => {
+export const wrapPageElement = ({ props }, THEME_OPTIONS) => {
+  const { password, pagePaths, partialMatching } = THEME_OPTIONS;
   const { location } = props;
-  const passwordCandidate = getQueryPassword(location) || getSessionPassword();
 
-  if (passwordCandidate === themeOptions.password) {
+  // password protection disabled
+  if (!password) {
     return;
   }
 
+  // page-level protection
+  const isPageLevelProtectionOn = pagePaths && pagePaths.length > 0;
+  if (isPageLevelProtectionOn) {
+    // non-protected page
+    if (!isProtectedPage(location, pagePaths, partialMatching)) {
+      return;
+    }
+  }
+
+  // correct password
+  const passwordCandidate = getQueryPassword(location) || getSessionPassword();
+  if (passwordCandidate === password) {
+    return;
+  }
+
+  // check password
   return <PasswordProtect />;
 };
